9028e4c8710af2922a527c72a45d0fcc810b741c
Emilio López <turl@tuxfamily.org>
2012-05-25 01:34:08 +0000

    Build only on Allwinner devices

--- Android.mk
+++ Android.mk
@@ -1,4 +1,9 @@
+# Only build on Allwinner devices
+ifeq ($(TARGET_BOARD_PLATFORM),sun4i)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
